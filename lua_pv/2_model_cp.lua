11f5b2304f4ddb99edb8de79267823c498533c20
EmilRex <emil.christensen@enigma.io>
2015-03-02 18:59:21 -0500

    Replaced puhrsch model with farabret

--- lua_pv/2_model_cp.lua
+++ lua_pv/2_model_cp.lua
@@ -16,14 +16,31 @@ require 'xlua';
 ----------------------------------------------------------------------
 print '==> define parameters'
 
-nfeats = 3        -- number of channels in input images
-nstates = 23      -- number of convolution kernels
-filtsize = 7      -- width and height of convolution kernels
-stepsize = 2      -- stride size for convolution and pooling
--- padding = 2       -- for padding
-noutputs = 10     -- number of output classes
-pooling = 2       -- width and height of pooling 
+-- nfeats = 3        -- number of channels in input images
+-- nstates = 23      -- number of convolution kernels
+-- filtsize = 7      -- width and height of convolution kernels
+-- stepsize = 2      -- stride size for convolution and pooling
+-- -- padding = 2       -- for padding
+-- noutputs = 10     -- number of output classes
+-- pooling = 2       -- width and height of pooling 
 drop_prob = 0.5   -- dropout probability (regularization)
+
+-- 10-class problem
+noutputs = 10
+
+-- input dimensions
+nfeats = 3
+width = 100
+height = 100
+ninputs = nfeats*width*height
+
+-- number of hidden units (for MLP only):
+nhiddens = ninputs / 2
+
+-- hidden units, filter sizes (for ConvNet only):
+nstates = {64,64,128}
+filtsize = 5
+poolsize = 2
 
 ----------------------------------------------------------------------
 -- Christian Puhrsch model architecture:
@@ -36,15 +53,32 @@ drop_prob = 0.5   -- dropout probability (regularization)
 model = nn.Sequential()
 -- model:add(nn.SpatialZeroPadding(padding, padding, padding, padding))
 if opt.type == 'cuda' then
-	model:add(nn.SpatialConvolutionMM(nfeats, nstates, filtsize, filtsize, stepsize, stepsize, padding))
-    model:add(nn.ReLU())
-    model:add(nn.SpatialMaxPooling(pooling, pooling, stepsize, stepsize))
+	-- model:add(nn.SpatialConvolutionMM(nfeats, nstates, filtsize, filtsize, stepsize, stepsize, padding))
+ --    model:add(nn.ReLU())
+ --    model:add(nn.SpatialMaxPooling(pooling, pooling, stepsize, stepsize))
     
-    model:add(nn.View(23*23*23))
-    model:add(nn.Dropout(drop_prob))
-    model:add(nn.Linear(23*23*23, 50))
+ --    model:add(nn.View(23*23*23))
+ --    model:add(nn.Dropout(drop_prob))
+ --    model:add(nn.Linear(23*23*23, 50))
+ --    model:add(nn.ReLU())
+ --    model:add(nn.Linear(50, noutputs))
+
+    -- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
+    model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
     model:add(nn.ReLU())
-    model:add(nn.Linear(50, noutputs))
+    model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
+
+    -- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
+    model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
+    model:add(nn.ReLU())
+    model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
+
+    -- stage 3 : standard 2-layer neural network
+    model:add(nn.View(nstates[2]*filtsize*filtsize))
+    model:add(nn.Dropout(0.5))
+    model:add(nn.Linear(nstates[2]*filtsize*filtsize, nstates[3]))
+    model:add(nn.ReLU())
+    model:add(nn.Linear(nstates[3], noutputs))
 
 else 
 	model:add(nn.SpatialConvolution(nfeats, nstates, filtsize, filtsize, stepsize, stepsize, padding))

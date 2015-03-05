f27746201410d952764f4a1e3e7809b2bfa95884
Priyank Bhatia <pb1672@nyu.edu>
2015-03-05 17:04:08 -0500

    Augment_2 added

---
+++ lua/augment_2.lua
@@ -0,0 +1,40 @@
+print '==> setting up augmentation'
+------ Set up size and count variables
+n_training = trainData:size() -- original training samples
+n_reflected = trainData:size() -- to be generated
+n_data = n_training + n_reflected
+n_rotated = trainData:size() -- to be generated
+
+n_channels = trainData.data:size()[2]
+w = trainData.data:size()[3] -- assuming square
+h = trainData.data:size()[4]
+
+------ Create new data and labels tensors to store original
+------ training data and augmented data
+data = torch.FloatTensor(n_data, n_channels, w, h)
+labels = torch.FloatTensor(n_data)
+
+------ Place original training data in new tensores
+data[{ {1,n_training} }] = trainData.data
+labels[{ {1,n_training} }] = trainData.labels
+labels[{ {n_training+1,n_training+n_reflected} }] = trainData.labels
+labels[{ {n_training+1,n_training+n_rotated} }] = trainData.labels
+
+print '==> creating reflections'
+------ Reflection
+for i = 1,n_training do
+--  image.hflip(data[{ {n_training+1, n_training+n_reflected} }], trainData.data)
+  image.rotate(data[{ {n_training+1, n_training+n_rotated} }], 0.35) 
+
+end
+--
+
+------ Point trainData.data and trainData.labels to new tensors
+trainData.data = data
+trainData.labels = labels
+
+-- Size variables are used in train and test functions
+trsize = trainData:size()
+tesize = testData:size()
+
+print '==> augmentation complete'
